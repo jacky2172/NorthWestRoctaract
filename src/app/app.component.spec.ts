f05200c9a21db6d5982b9a9be3adc3cefd6a843c
Jacky.Lin <jackylin@hitrust.com.tw>
2023-06-25 01:23:17 +0800

    rename north-west-roctaract -> north-west-rotaract

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'north-west-roctaract'`, () => {
+  it(`should have as title 'north-west-rotaract'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('north-west-roctaract');
+    expect(app.title).toEqual('north-west-rotaract');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('north-west-roctaract app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('north-west-rotaract app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'north-west-roctaract';
+  title = 'north-west-rotaract';
 }
